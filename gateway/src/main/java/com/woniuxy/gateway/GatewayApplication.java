c4379f14f20aa9cdbfad991707d51c1c34d4899f
Qfx1511 <1156634036@qq.com>
2022-06-06 20:18:09 +0800

    init

--- gateway/src/main/java/com/woniuxy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/woniuxy/gateway/GatewayApplication.java
@@ -3,13 +3,12 @@ package com.woniuxy.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
+@EnableFeignClients("com.woniuxy.commons.service")
 @ComponentScan(basePackages = "com.woniuxy",
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.REGEX,
