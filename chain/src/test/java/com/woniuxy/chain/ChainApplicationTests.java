144ae9bbf8886aed8254410aa7e8c0d24041526a
DaiCode <827576937@qq.com>
2022-06-07 19:24:14 +0800

    [新增] [新增] 还款、支付利息、手续费

--- chain/src/test/java/com/woniuxy/chain/ChainApplicationTests.java
+++ chain/src/test/java/com/woniuxy/chain/ChainApplicationTests.java
@@ -1,13 +1,22 @@
 package com.woniuxy.chain;
 
+import com.woniuxy.chain.service.ScfpChainService;
+import com.woniuxy.commons.entity.PageInfomation;
+import com.woniuxy.commons.entity.ResponseResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class ChainApplicationTests {
 
+    @Resource
+    ScfpChainService service;
     @Test
     void contextLoads() {
+        ResponseResult<Object> all = service.findAll(new PageInfomation(1, 5));
+        System.out.println(all);
     }
 
 }
--- loan/src/main/java/com/woniuxy/loan/LoanApplication.java
+++ loan/src/main/java/com/woniuxy/loan/LoanApplication.java
@@ -1,9 +1,13 @@
 package com.woniuxy.loan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.woniuxy.commons.service")
+@MapperScan("com.woniuxy.loan.dao")
 public class LoanApplication {
 
     public static void main(String[] args) {
