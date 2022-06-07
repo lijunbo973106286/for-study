4a6e49bd065519fd6929d9aa5d5e16e5e400168c
ZJH <403575975@qq.com>
2022-06-07 14:38:46 +0800

    新增链单findAll

--- chain/src/main/java/com/woniuxy/chain/service/impl/ScfpChainServiceImpl.java
+++ chain/src/main/java/com/woniuxy/chain/service/impl/ScfpChainServiceImpl.java
@@ -1,12 +1,18 @@
 package com.woniuxy.chain.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.woniuxy.chain.dao.ScfpChainDao;
 import com.woniuxy.chain.service.ScfpChainService;
+import com.woniuxy.commons.entity.PageInfomation;
+import com.woniuxy.commons.entity.ResStatus;
 import com.woniuxy.commons.entity.ResponseResult;
 import com.woniuxy.commons.entity.ScfpChain;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * \* @author: ZJH
@@ -36,6 +42,20 @@ public class ScfpChainServiceImpl implements ScfpChainService {
             return ResponseResult.SUCCESS;
         } else {
             return ResponseResult.FAIL;
+        }
+    }
+
+    @Override
+    public ResponseResult<Object> findAll(PageInfomation pageInfomation) {
+        int currentPage = pageInfomation.getCurrentPage();
+        int pageSize = pageInfomation.getPageSize();
+        PageHelper.startPage(currentPage,pageSize);
+        List<ScfpChain> all = scfpChainDao.findAll();
+        if(all.isEmpty()){
+            return new ResponseResult(500, "查询失败", null, ResStatus.FAIL);
+        }else{
+            PageInfo<ScfpChain> info = PageInfo.of(all);
+            return new ResponseResult(200, "查询成功", info, ResStatus.SUCCESS);
         }
     }
 }
