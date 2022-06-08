2631af616636088f203a38239171e7358652f11e
xxxps <997866143@qq.com>
2022-06-08 16:59:34 +0800

    财务中心>手续费

---
+++ fiance/src/main/java/com/woniuxy/fiance/mapper/settleMapper.java
@@ -0,0 +1,18 @@
+package com.woniuxy.fiance.mapper;
+
+import com.woniuxy.commons.entity.DTO.LoanDTO;
+import com.woniuxy.commons.entity.ScfpLoan;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface settleMapper {
+    public List<LoanDTO> findsettle(int id) ;
+
+    List<LoanDTO> Paid(int id);
+
+    List<LoanDTO> unpaid(int id);
+
+    List<LoanDTO> search(LoanDTO loanDTO);
+}
---
+++ fiance/src/main/java/com/woniuxy/fiance/service/settleService.java
@@ -0,0 +1,14 @@
+package com.woniuxy.fiance.service;
+
+import com.woniuxy.commons.entity.DTO.LoanDTO;
+import com.woniuxy.commons.util.ResponseResult;
+
+public interface settleService {
+    ResponseResult findsettle(int currentPate, int pageSize, int id);
+
+    ResponseResult Paid(int currentPate, int pageSize, int id);
+
+    ResponseResult unpaid(int currentPate, int pageSize, int id);
+
+    ResponseResult<LoanDTO> search(LoanDTO loanDTO);
+}
